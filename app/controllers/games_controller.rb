03618c2f3d7c868b786e0c48393510021e0c0802
Rhys Woodberry <rhysbrian@gmail.com>
2018-05-04 14:00:12 +1000

    change authenticate user routes

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,6 @@
 class GamesController < ApplicationController
   before_action :set_game, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
   # GET /games
   # GET /games.json
   def index

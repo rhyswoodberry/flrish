a16219217d6c05b7bbc96461a8add8646c0aba69
Rhys Woodberry <rhysbrian@gmail.com>
2018-05-04 13:22:00 +1000

    continue devise installation

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :games
 
-  root 'games#index'
+  root to: 'games#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

929c713beff532ca1c3e8b682bb6a2027a542649
Rhys Woodberry <rhysbrian@gmail.com>
2018-05-04 15:48:41 +1000

    add welcome change landing page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :games
 
-  root to: 'games#index'
+  get '/welcome', to: 'welcome#index'
+
+  root to: 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

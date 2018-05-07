13ffab2da54e1b6bbd5cf27b2867a6379c963470
Rhys Woodberry <rhysbrian@gmail.com>
2018-05-07 10:44:40 +1000

    mount and configure  uploader

--- app/uploaders/cover_uploader.rb
+++ app/uploaders/cover_uploader.rb
@@ -13,6 +13,14 @@ class CoverUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
+
+  def content_type_whitelist
+    /image\//
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:

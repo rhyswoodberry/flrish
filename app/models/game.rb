ea533f8b61f6ab05a741e099d63480a2909476f8
Rhys Woodberry <rhysbrian@gmail.com>
2018-05-07 18:12:14 +1000

    change max file size

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,5 +2,5 @@ class Game < ApplicationRecord
   belongs_to :user
   mount_uploader :cover, CoverUploader
   validates :title, :description, :price, :cover, presence: true
-  validates :cover, file_size: { less_than: 1.megabytes }
+  validates :cover, file_size: { less_than: 3.megabytes }
 end

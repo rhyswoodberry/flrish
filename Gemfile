8d1813d5bea9309e7ddfd053f4e7e70150b550a9
Rhys Woodberry <rhysbrian@gmail.com>
2018-05-07 18:16:32 +1000

    add thumb and small thumb image resizing

--- Gemfile
+++ Gemfile
@@ -36,6 +36,8 @@ gem "figaro"
 gem 'carrierwave', '~> 1.0'
 # File validators
 gem 'file_validators'
+# Image Manipulations
+gem 'mini_magick'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/uploaders/cover_uploader.rb
+++ app/uploaders/cover_uploader.rb
@@ -1,7 +1,7 @@
 class CoverUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -37,9 +37,13 @@ class CoverUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+  version :thumb do
+    process resize_to_fit: [50, 50]
+  end
+
+  version :small_thumb, from_version: :thumb do
+    process resize_to_fill: [20, 20]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:

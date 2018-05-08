4bc3d6d25baa083e00ec659d6d15156c656a9db0
Rhys Woodberry <rhysbrian@gmail.com>
2018-05-08 15:45:33 +1000

    fix associations

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,5 @@
 class Game < ApplicationRecord
-  belongs_to :user
+  belongs_to :developer, class_name: 'User', foreign_key: :developer_id
   mount_uploader :cover, CoverUploader
   validates :title, :description, :price, :cover, presence: true
   validates :cover, file_size: { less_than: 3.megabytes }

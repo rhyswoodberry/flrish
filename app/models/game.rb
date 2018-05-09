f40afee8710c338f203f86052d3a0393474d4af9
Rhys Woodberry <rhysbrian@gmail.com>
2018-05-09 12:27:13 +1000

    test validations part two

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,6 @@
 class Game < ApplicationRecord
   belongs_to :user
   mount_uploader :cover, CoverUploader
-  # validates :title, :description, :price, :cover, presence: true
+  validates :title, :description, :price, presence: true
   validates :cover, file_size: { less_than: 3.megabytes }
 end

b9c654be70b80137f98b2fbaaed4a2edf70fb5da
Rhys Woodberry <rhysbrian@gmail.com>
2018-05-09 17:35:34 +1000

    configure stripe

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,10 +1,13 @@
 class ChargesController < ApplicationController
+  before_action :set_item
+
   def new
   end
   
   def create
     # Amount in cents
-    @amount = 500
+    @amount = @game.price * 100
+    @amount = @amount.to_i
   
     customer = Stripe::Customer.create(
       :email => params[:stripeEmail],
@@ -23,4 +26,9 @@ class ChargesController < ApplicationController
     redirect_to new_charge_path
   end
   
+  private
+  def set_item
+    @game = Game.find(params[:game_id])
+  end
+
 end

a4a74ed1201f539930911f7f551875c529c1bb46
Rhys Woodberry <rhysbrian@gmail.com>
2018-05-10 11:00:57 +1000

    add rails helper and write game model test

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "game is only valid if a user exists" do
+    user = User.create!(email: 'example@example.com', password: '123456')
+    game = Game.create!(title: 'EG', description: 'Example', price: '10.99', cover: 'example.png')
+    expect Game.to eq([:title, :description, :price, :cover, :user_id])
+  end
 end
